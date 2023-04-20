64697d65b1269f34d5495246beddb49c36b25b98
jorgejsolorzano <jorgej.solorzano@gmail.com>
2023-04-20 15:00:18 -0500

    modificando el reporte patient_card_report, los encabezados 20-04-2023

--- addons/basic_hms/__manifest__.py
+++ addons/basic_hms/__manifest__.py
@@ -93,6 +93,7 @@
         'report/patient_medications_document_report.xml',
         'report/patient_vaccinations_document_report.xml',
         'report/prescription_demo_report.xml',
+
     ],
     "author": "BrowseInfo",
     "website": "https://www.browseinfo.in",
